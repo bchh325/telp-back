fa920ca3adfce149e5450ca10a824b47add19dc5
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2024-12-24 12:32:27 -0800

    set up spring web api

---
+++ src/main/java/com/example/telpback/IdentityPlatformController.java
@@ -0,0 +1,15 @@
+package com.example.telpback;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IdentityPlatformController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "test";
+    }
+
+}
