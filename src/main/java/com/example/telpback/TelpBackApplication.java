5f6c6f88241f02899d853548841ad9ea777f723e
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2024-12-24 10:21:55 -0800

    initial commit

---
+++ src/main/java/com/example/telpback/TelpBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.telpback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelpBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelpBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/telpback/TelpBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telpback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelpBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
