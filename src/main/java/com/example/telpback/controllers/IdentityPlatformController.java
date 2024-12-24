03a63b936444aae5334f0cb80569ebc033fb5878
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2024-12-24 13:55:48 -0800

    refactoring

--- src/main/java/com/example/telpback/controllers/IdentityPlatformController.java
+++ src/main/java/com/example/telpback/controllers/IdentityPlatformController.java
@@ -1,4 +1,4 @@
-package com.example.telpback;
+package com.example.telpback.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
