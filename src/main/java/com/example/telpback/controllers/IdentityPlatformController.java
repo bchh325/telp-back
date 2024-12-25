224e4e61bb3111f68a56b4854c23fe89dff0397c
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2024-12-24 18:40:19 -0800

    set up JSON responses

--- src/main/java/com/example/telpback/controllers/IdentityPlatformController.java
+++ src/main/java/com/example/telpback/controllers/IdentityPlatformController.java
@@ -1,15 +1,19 @@
 package com.example.telpback.controllers;
 
+import com.example.telpback.models.IdentityPlatform;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
 
 @RestController
 public class IdentityPlatformController {
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return "deployment testing1" + name;
+    public IdentityPlatform greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new IdentityPlatform("Testing Response");
     }
 
 }
---
+++ src/main/java/com/example/telpback/models/IdentityPlatform.java
@@ -0,0 +1,17 @@
+package com.example.telpback.models;
+
+public class IdentityPlatform {
+    private String response;
+
+    public IdentityPlatform(String response) {
+        this.response = response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+}
