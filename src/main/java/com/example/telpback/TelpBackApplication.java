0b279830c75ebe6f82c4fd7e460a7613044e5de1
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2025-02-09 22:45:35 -0800

    config

--- src/main/java/com/example/telpback/TelpBackApplication.java
+++ src/main/java/com/example/telpback/TelpBackApplication.java
@@ -1,10 +1,23 @@
 package com.example.telpback;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileInputStream;
+
 @SpringBootApplication
 public class TelpBackApplication {
+
+    FileInputStream serviceAccount = new FileInputStream("telpServiceAccount.json");
+
+    FirebaseOptions options = new FirebaseOptions.Builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .build();
+
+    FirebaseApp.initializeApp(options);
 
     public static void main(String[] args) {
         SpringApplication.run(TelpBackApplication.class, args);
