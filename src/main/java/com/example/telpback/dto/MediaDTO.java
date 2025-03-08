8a085a180af5f25980ce8afc1d8a4f1cb03ce515
bchh325 <bbchhorb@gmail.com>
2025-03-08 13:35:07 -0800

    update

--- src/main/java/com/example/telpback/dto/MediaDTO.java
+++ src/main/java/com/example/telpback/dto/MediaDTO.java
@@ -1,11 +1,42 @@
 package com.example.telpback.dto;
 
 public class MediaDTO {
-
-    public interface Media
     public MediaDTO() {}
 
     private String userId;
     private String mimeType;
+    private String placeId;
 
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    public String getPlaceId() {
+        return placeId;
+    }
+
+    public void setPlaceId(String placeId) {
+        this.placeId = placeId;
+    }
+
+    @Override
+    public String toString() {
+        return "MediaDTO{" +
+                "userId='" + userId + '\'' +
+                ", mimeType='" + mimeType + '\'' +
+                ", placeId='" + placeId + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/example/telpback/models/Picture.java
+++ src/main/java/com/example/telpback/models/Picture.java
@@ -1,11 +1,14 @@
 package com.example.telpback.models;
 
+import com.example.telpback.dto.MediaDTO;
 import com.google.cloud.Timestamp;
 import com.google.cloud.firestore.FieldValue;
 import com.google.cloud.firestore.annotation.ServerTimestamp;
 import org.springframework.web.multipart.MultipartFile;
 
 public class Picture {
+    private String userId;
+    private String mimeType;
     private String placeId;
 
     @ServerTimestamp
@@ -14,7 +17,7 @@ public class Picture {
     public Picture() {
     }
 
-    public Picture(String placeId) {
+    public Picture(MediaDTO mediaDto) {
         this.placeId = placeId;
     }
 
@@ -32,6 +35,22 @@ public class Picture {
 
     public void setTimestamp(Timestamp timestamp) {
         this.timestamp = timestamp;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
     }
 
     @Override
